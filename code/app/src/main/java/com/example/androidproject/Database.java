97c97326e2c9f60518b55ebefbd18bf486e4c74b
Rhiyon <rhiyon132@gmail.com>
2025-02-26 14:27:19 -0700

    Added common database functionality and changed kts files
    Rhiyon adding common Database
    
    Changed compile sdk from 34 to 35 since it said I needed to, as well as the sdk target from 34 to 35 in build.gradle.kts(app) Al

--- code/app/src/main/java/com/example/androidproject/Database.java
+++ code/app/src/main/java/com/example/androidproject/Database.java
@@ -4,18 +4,38 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Creates a database class to allow interacting with the database
  */
 public class Database {
     private FirebaseFirestore database;
+    private static Database dbInstance; // Used to make sure there is only a singular instance of the database throughout all classes
     private CollectionReference moods;
     private CollectionReference users;
 
-    public Database(){
+
+    /**
+     * Constructor called on by getInstance()
+     */
+    private Database(){
         database = FirebaseFirestore.getInstance();
         moods = database.collection("Moods");
         users = database.collection("Users");
     }
+
+    /**
+     * Creates an instance of the dataBase if it does not exists.
+     * Otherwise returns the existing instance of it.
+     * To be used in other classes to allow them to access the database
+     * @return instance of the Database.
+     */
+    public static Database getInstance(){
+        if (dbInstance == null) {
+            dbInstance = new Database();
+        } return dbInstance;
+    }
+
 }
