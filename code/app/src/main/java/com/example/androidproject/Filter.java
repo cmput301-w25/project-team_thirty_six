4777b51cfab6dcec3d3abba2497ddfcf0dd7ad60
azya404 <haasyashah404@gmail.com>
2025-03-28 04:45:23 -0600

    Finally done with the 5k user story

--- code/app/src/main/java/com/example/androidproject/Filter.java
+++ code/app/src/main/java/com/example/androidproject/Filter.java
@@ -1,8 +1,12 @@
 package com.example.androidproject;
 
+import android.location.Location;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Filter {
 
@@ -54,6 +58,47 @@ public class Filter {
         for (MoodState mood : moodHistory) {
             if (mood.getReason() != null && mood.getReason().toLowerCase().contains(keyword.toLowerCase())) {
                 filteredMoods.add(mood);
+            }
+        }
+        return filteredMoods;
+    }
+
+    public static ArrayList<MoodState> filterBy5kDistance(ArrayList<MoodState> moodHistory, Location currentLocation, List<String> followingList, double radius) {
+        if (currentLocation == null || followingList == null || followingList.isEmpty()) {
+            return new ArrayList<>(); //location permission denied or user isnt following anyone then the results is basically null/empty
+        }
+        ArrayList<MoodState> filteredMoods = new ArrayList<>(); //a store for moods that may fit the criteria
+        // start by checking the radius relative to users current position as well as the users we follow
+        ArrayList<MoodState> candidateMoods = new ArrayList<>();
+        for (MoodState mood : moodHistory) {
+            if (mood.getLocation() != null && followingList.contains(mood.getUser())) {
+                // Calculate distance
+                // Taken from https://stackoverflow.com/questions/22751306/android-location-distancebetween-and-using-previous-location/22755930#22755930
+                // Taken from https://stackoverflow.com/questions/6510050/android-location-distancebetween-and-location-distanceto-difference
+                // By: Haasya
+                float[] distance = new float[1];
+                Location.distanceBetween(
+                        currentLocation.getLatitude(), currentLocation.getLongitude(),
+                        mood.getLocation().getLatitude(), mood.getLocation().getLongitude(),
+                        distance
+                );
+
+                // fix added: needed to convert it to kilometers
+                if (distance[0] / 1000 <= radius) {
+                    candidateMoods.add(mood);
+                }
+            }
+        }
+        ArrayList<String> processedUsers = new ArrayList<>(); // we use this to store if the user has been processed (like a boolean)
+        Collections.sort(candidateMoods, (m1, m2) ->
+                m2.getDayTime().compareTo(m1.getDayTime())); // this sorts the mood in desc order so that each users latest mood is first
+
+        // only need to add the most recent mood for each user we are following
+        for (MoodState mood : candidateMoods) {
+            String username = mood.getUser();
+            if (!processedUsers.contains(username)) {
+                filteredMoods.add(mood);
+                processedUsers.add(username);
             }
         }
         return filteredMoods;
